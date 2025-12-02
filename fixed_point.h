7b94e84f1b1c17b64c52f380911a5925bbe07abe
racheldean1 <racheldean1@arizona.edu>
2025-12-02 13:17:13 -0700

    Update fixed_point.h

--- fixed_point.h
+++ fixed_point.h
@@ -1,5 +1,5 @@
 #ifndef FIXED_POINT_H
 #define FIXED_POINT_H
 int fixed_point_solve(double x1_0, double x2_0, double tol, int max_iter,
-                      double *x1_out, double *x2_out, int *iters_out);
+                      double *x1_out, double *x2_out, int *iters_out, double scale);
 #endif
